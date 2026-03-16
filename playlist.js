8c4202560654c572cfeb56ba1c51cc335a52805b
GUTO CHRISTIANINI <43747810+gutojau@users.noreply.github.com>
2026-03-16 19:13:00 -0300

    Add files via upload

--- playlist.js
+++ playlist.js
@@ -1,9 +1,6 @@
 window.PLAYLIST_FILES = [
   "0-vinheta.mp4"
  ,"1-vinhera.mp4"
- ,"Geração_de_Vídeo_e_Continuação.mp4"
  ,"odontologia Berro - fonte certa.mp4"
  ,"odontologia berro.mp4"
- ,"video 3.mp4"
- ,"Vídeo_Pronto_Incubadora_Virtual_Inovacps.mp4"
 ];
